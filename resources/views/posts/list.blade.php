0d66457fa15a48e14c86e33e717c6226ae459671
Vladymyr <vovangud@gmail.com>
2022-09-24 16:15:35 +0200

    Finish updates

--- resources/views/posts/list.blade.php
+++ resources/views/posts/list.blade.php
@@ -7,8 +7,10 @@
     <table class="table table-bordered table-hover table-dark">
         <thead>
         <tr>
-            <td colspan="2"  style="text-align: center;"><a href="../" style="font-size: 20px; color: #ffc107">&#11152; back</a></td>
-            <td colspan="7"  style="text-align: center;"><a href="create-post.php" style="font-size: 20px; color: #ffc107"> Create Post</a></td>
+            <td colspan="2" style="text-align: center;"><a href="../" style="font-size: 20px; color: #ffc107">&#11152;
+                    back</a></td>
+            <td colspan="7" style="text-align: center;"><a href="create-post.php"
+                                                           style="font-size: 20px; color: #ffc107"> Create Post</a></td>
         </tr>
         </thead>
         <thead>
@@ -34,8 +36,10 @@
                 <td>{{ $post->category_id}}</td>
                 <td>{{ $post->created_at }}</td>
                 <td>{{ $post->updated_at }}</td>
-                <td><a class="btn btn-success btn-sm" href="update-post.php?id={{ $post->id }}">&#9999;<i class="fa fa-edit"></i></a></td>
-                <td><a class="btn btn-light btn-sm" href="delete-post.php?id={{ $post->id }}">&#10060;<i class="fa fa-trash"></i></a></td>
+                <td><a class="btn btn-success btn-sm" href="update-post.php?id={{ $post->id }}">&#9999;<i
+                                class="fa fa-edit"></i></a></td>
+                <td><a class="btn btn-light btn-sm" href="delete-post.php?id={{ $post->id }}">&#10060;<i
+                                class="fa fa-trash"></i></a></td>
             </tr>
         @endforeach
         </tbody>
