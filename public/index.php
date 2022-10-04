8d91489c148396cea4c133cf16f25cda505848b5
Vladymyr <vovangud@gmail.com>
2022-10-04 22:58:57 +0300

    Finish Project

--- public/index.php
+++ public/index.php
@@ -1,10 +1,5 @@
 <?php
 require_once '../vendor/autoload.php';
-//require_once '../config/dotenv.php';
-//require_once '../config/database.php';
-//require_once '../config/blade.php';
-//require_once '../config/validator.php';
-//require_once '../config/router.php';
 
 use Vagrant\Lesson11\Models\PizzaMexico;
 use Vagrant\Lesson11\Models\PizzaChickenGrill;
